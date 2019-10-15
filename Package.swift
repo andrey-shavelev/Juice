877ffb415de24a9413529be1b2e0d9a48e46c5b6
Andrey Shavelev <shavelev.andrey@gmail.com>
2019-10-15 19:07:18 +0300

    Renaming package juice -> Juice

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-        name: "juice",
+        name: "Juice",
         products: [
             // Products define the executables and libraries produced by a package, and make them visible to other packages.
             .library(
-                    name: "juice",
-                    targets: ["juice"]),
+                    name: "Juice",
+                    targets: ["Juice"]),
         ],
         dependencies: [
             // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
             .target(
-                    name: "juice",
+                    name: "Juice",
                     dependencies: []),
             .testTarget(
-                    name: "juiceTests",
-                    dependencies: ["juice"]),
+                    name: "JuiceTests",
+                    dependencies: ["Juice"]),
         ]
 )
