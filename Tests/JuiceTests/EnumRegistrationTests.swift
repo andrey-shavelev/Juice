66c2f387b04bf5ccc1bedc36047ff95ae74d4a65
Andrey Shavelev <andrey.shavelev@icloud.com>
2020-05-03 10:33:55 +0300

    Add enum registration tests

---
+++ Tests/JuiceTests/EnumRegistrationTests.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Andrey Shavelev on 03/05/2020.
+//
+import XCTest
+import Juice
+
+final class EnumRegistrationsTest: XCTestCase {
+    func canRegisterAndResolveEnumValue() throws {
+        let container = try Container { builder in
+            builder.register(value: TestEnum.three)
+                .as(TestEnum.self)
+        }
+        
+        let actualValue = try container.resolve(TestEnum.self)
+        
+        XCTAssertEqual(TestEnum.three, actualValue)
+    }
+}
